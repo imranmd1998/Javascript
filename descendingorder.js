766f894fc430b3ef267d8c44d1dbb29e054090f0
MD IMRAN <107649998+imranmd1998@users.noreply.github.com>
2023-10-14 21:44:51 +0530

    Add files via upload

---
+++ descendingorder.js
@@ -0,0 +1,9 @@
+const array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+
+//comparison function to sort in descending order.
+array.sort((a,b) =>b-a);
+
+console.log(array); 
+
+
+// Output: [9, 6, 5, 5, 5, 4, 3, 3, 2, 1, 1]
